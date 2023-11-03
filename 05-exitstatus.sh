417ba34a8f4d420ac42d5bce9e875a21811ee9bd
YMaheswarReddy145 <yettapumahesh398@gmail.com>
2023-11-03 18:36:01 +0530

    Exit Status Topic

---
+++ 05-exitstatus.sh
@@ -0,0 +1,17 @@
+# Every command we run on the shell returns some status about it. This is called exit status of the command.
+# Exit status is  number that ranges form 0 - 255.
+# These numbers are categorized as success(0) and failure(1-255).
+# To refer exit status of a last command  executed we can refer a variable ?.
+ls
+
+#To print the status we cn use echo $? immediately once  a command.
+echo $?
+
+# suppose if the give invalid data (i..e, file name) then it show  the exit status code from 1 -255
+lss
+echo $?
+
+# when we want ot stop the script we can use exit command.
+# exit command is returning a status 0 by default.
+exit
+
