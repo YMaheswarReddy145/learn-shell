2737368041c213c5bc0ed402253bb04cef6f8f21
YMaheswarReddy145 <yettapumahesh398@gmail.com>
2023-11-06 18:40:02 +0530

    Expense Project

---
+++ 06-functions.sh
@@ -0,0 +1,15 @@
+# If we assign a -name to a set of commands then it is called as functions.
+
+print_hello()
+{
+  echo Hello World
+}
+
+# Function names (print_hello) should be using alphabets & numbers & _(underscore)
+# Simply using function name in script, We can call the function
+# Functions can access variables of main program and vice-versa
+
+
+# The main usage of the function is when we have some repetetive commands then we can declare tht in the function and
+   # we can use that function reference every where. Advantage is, if we change in place that inputs all the places where
+   # that commands fre used.
